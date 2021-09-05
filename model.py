1603e1a50abd989637475ca7026a745b2634c7a4
lyyraaa <do206464@gmail.com>
2021-09-05 22:24:37 +0100

    add UI and functionality to change colors

--- model.py
+++ model.py
@@ -12,6 +12,14 @@ from attractor import Lorenz,Rossler,Liuchen,Thomas,NewtonLeipnik,Halvorsen,Dequ
 import argparse
 import sys
 
+import pyglet_gui
+from pyglet_gui.theme import Theme
+from pyglet_gui.manager import Manager
+from pyglet_gui.buttons import Button, OneTimeButton
+from pyglet_gui.containers import VerticalContainer, HorizontalContainer, Spacer
+from pyglet_gui.sliders import HorizontalSlider
+from pyglet_gui.gui import Label
+
 
 class Model:
 
@@ -65,7 +73,7 @@ class Model:
                 sys.exit(0)
 
 
-            self.line_list.append(pyglet.graphics.vertex_list(self.lim_per_att, 'v3f/stream', 'c4B/static'))#c3B/static
+            self.line_list.append(pyglet.graphics.vertex_list(self.lim_per_att, 'v3f/stream', 'c4B/dynamic'))#c3B/static
             self.point_list.append([0,0,0]*self.lim_per_att)
             self.point_list[att][:3] = (x,y,z)
 
@@ -93,11 +101,21 @@ class Model:
         for att in range(self.attC):
             self.line_list[att].draw(pyglet.gl.GL_LINE_STRIP)
 
+    def get_limperat(self):
+        return self.lim_per_att
+
+    def change_col(self,col_list):
+        self.color_list = col_list
+        for att in range(self.attC):
+            self.line_list[att].colors = self.color_list
+
 
 class Player:
     def __init__(self, pos=(0, 0, 0), rot=(0, 0)):
         self.pos = list(pos)
         self.rot = list(rot)
+        self.initpos = list(pos)
+        self.initrot = list(rot)
 
     def mouse_motion(self, dx, dy):
         dx/= 8
@@ -132,6 +150,16 @@ class Player:
         if keys[key.LCTRL]:
             self.pos[1] -= s*sens
 
+    def reset(self):
+        self.pos = self.initpos[:]
+        self.rot = self.initrot[:]
+
+    def getPos(self):
+        return self.pos
+
+    def getRot(self):
+        return self.rot
+
 class Window(pyglet.window.Window):
 
     def push(self,pos,rot):
@@ -157,12 +185,15 @@ class Window(pyglet.window.Window):
 
     def set3d(self):
         self.Projection()
-        gluPerspective(70, self.width/self.height, 0.05, 1000)
+        gluPerspective(self.fov, self.width/self.height, 0.05, 1000)
         self.Model()
 
     def setLock(self, state):
         self.lock = state
         self.set_exclusive_mouse(state)
+
+    def set_fov(self,fov):
+        self.fov = fov
 
     lock = False
     mouse_lock = property(lambda self:self.lock, setLock)
@@ -173,6 +204,7 @@ class Window(pyglet.window.Window):
         self.keys = key.KeyStateHandler()
         self.push_handlers(self.keys)
         pyglet.clock.schedule(self.update)
+        self.fov=70
 
         arglist = vars(arglist)
 
@@ -204,6 +236,130 @@ class Window(pyglet.window.Window):
         self.push(self.player.pos,self.player.rot)
         self.model.draw()
         glPopMatrix()
+
+    def getPlayer(self):
+        return self.player
+
+class WindowUI(pyglet.window.Window):
+
+    def __init__(self, window3D, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.window3D = window3D
+
+        self.batch = pyglet.graphics.Batch()
+
+        self.theme = Theme(
+            {"font": "Lucida Grande",
+            "font_size": 12,
+            "text_color": [255, 255, 255, 255],
+            "gui_color": [255, 255, 255, 255],
+            "button": {
+                "text_color": [0, 0, 0, 255],
+                "down": {
+                    "image": {
+                        "source": "button-down.png",
+                        "frame": [8, 6, 2, 2],
+                        "padding": [18, 18, 8, 6]
+                        },
+                    },
+                "up": {
+                    "image": {
+                        "source": "button.png",
+                        "frame": [6, 5, 6, 3],
+                        "padding": [18, 18, 8, 6]
+                        }
+                    }
+                },
+            "slider": {
+                   "knob": {
+                       "image": {
+                           "source": "slider-knob.png"
+                       },
+                       "offset": [-5, -11]
+                   },
+                   "padding": [8, 8, 8, 8],
+                   "step": {
+                       "image": {
+                           "source": "slider-step.png"
+                       },
+                       "offset": [-2, -8]
+                   },
+                   "bar": {
+                       "image": {
+                           "source": "slider-bar.png",
+                           "frame": [8, 8, 8, 0],
+                           "padding": [8, 8, 8, 8]
+                       }
+                   }
+               }
+            }, resources_path='theme/')
+
+        label_fov = Label('Change FOV')
+        label_col = Label('Change Colour')
+        label_position = Label('Player position\nand view angle')
+        self.label_pos = Label('postionsplaceholder')
+        self.label_rot = Label('rotationplaceholder')
+
+        button_reset = OneTimeButton('RESET POS', on_release=self.callback)
+        # FOR TOGGLE BUTTONS USE Button not OneTimeButton, and on_release -> on_press
+
+        button_col = OneTimeButton('CHANGE COL', on_release=self.change_col)
+
+
+        self.sliderFOV = HorizontalSlider(on_set=self.change_fov)
+
+        self.sliderR = HorizontalSlider()
+        self.sliderG = HorizontalSlider()
+        self.sliderB = HorizontalSlider()
+
+        container_pos = VerticalContainer([label_position, self.label_pos, self.label_rot])
+        container_fov = VerticalContainer([label_fov, self.sliderFOV, button_reset])
+        container_col = VerticalContainer([label_col, self.sliderR, self.sliderG, self.sliderB, button_col])
+
+        self.manager = Manager(\
+            content=HorizontalContainer([Spacer(10),container_pos, Spacer(25),container_fov, Spacer(25), container_col]),
+            window=self,
+            theme=self.theme,
+            batch=self.batch,
+            is_movable=False)
+
+
+    def on_draw(self):
+        self.clear()
+        self.batch.draw()
+
+        self.update_labels()
+
+
+    def update_labels(self):
+        posinfo = ["{:.2f}".format(s) for s in self.window3D.player.getPos()]
+        rotinfo = ["{:.2f}".format(s) for s in self.window3D.player.getRot()]
+        self.label_pos.set_text("x: " + posinfo[0] + " y: " + posinfo[1] + " z: " + posinfo[2])
+        self.label_rot.set_text("V: " + rotinfo[0] + " H: " + rotinfo[1])
+
+    def callback(self,is_pressed):
+        self.window3D.set_fov(75)
+        self.window3D.getPlayer().reset()
+
+    def change_col(self,is_pressed):
+        limp = self.window3D.model.get_limperat()
+        r,g,b = int(255*self.sliderR.value),int(255*self.sliderG.value),int(255*self.sliderB.value)
+        col_list = []
+        for place in range(1,limp+1):
+            col_list.append(r)
+            col_list.append(g)
+            col_list.append(b)
+            col_list.append(int(255*(place/limp)))
+
+        self.window3D.model.change_col(col_list)
+
+    def change_fov(self,is_pressed):
+        self.window3D.set_fov(int(65 + self.sliderFOV.value*45))
+
+    def on_close(self):
+        self.window3D.close()
+        self.close()
 
 
 
@@ -238,10 +394,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    window = Window(width=400, height=300, caption='My caption',resizable=True,arglist=args)
+    window = Window(width=600, height=450, caption='My caption',resizable=True,arglist=args)
+
 
     pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
     pyglet.gl.glBlendFunc(pyglet.gl.GL_SRC_ALPHA, pyglet.gl.GL_ONE_MINUS_SRC_ALPHA)
     glClearColor(0,0,0,1)
     glEnable(GL_DEPTH_TEST)
+
+    windowUI = WindowUI(window,width=1000, height=400, caption='UI',resizable=True)
     pyglet.app.run()
